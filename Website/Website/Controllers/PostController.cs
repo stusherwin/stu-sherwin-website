4a39f4a8bac09145740a73eee7e94fe88bb1f819
stu.sherwin <stu.sherwin@googlemail.com>
2011-04-29 10:08:41 +0000

    Added CK Editor, styling

---
+++ Website/Website/Controllers/PostController.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Website.Models;
+using System.Data;
+
+namespace Website.Controllers
+{
+    public class PostController : Controller
+    {
+        //
+        // GET: /Post/
+
+        public ActionResult Index()
+        {
+            var context = new Entities();
+            var posts = context.Posts.ToArray();
+            return View(posts);
+        }
+
+        public ActionResult Create()
+        {
+            return View(new Post());
+        }
+
+        [HttpPost]
+        [ValidateInput(false)]
+        public ActionResult Create(Post post)
+        {
+            var context = new Entities();
+            context.Posts.Add(post);
+            context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult Display(int id)
+        {
+            var context = new Entities();
+            var post = context.Posts.FirstOrDefault(p => p.Id == id);
+            return View(post);
+        }
+
+        public ActionResult Edit(int id)
+        {
+            var context = new Entities();
+            var post = context.Posts.FirstOrDefault(p => p.Id == id);
+            return View(post);
+        }
+
+        [HttpPost]
+        [ValidateInput(false)]
+        public ActionResult Edit(Post post)
+        {
+            var context = new Entities();
+            context.Entry(post).State = EntityState.Modified;
+            context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult Delete(int id)
+        {
+            var context = new Entities();
+            var post = context.Posts.FirstOrDefault(p => p.Id == id);
+            return View(post);
+        }
+
+        [HttpPost]
+        public ActionResult Delete(Post post)
+        {
+            var context = new Entities();
+            context.Entry(post).State = EntityState.Deleted;
+            context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+    }
+}
--- Website/Website/Global.asax.cs
+++ Website/Website/Global.asax.cs
@@ -24,7 +24,7 @@ namespace Website
             routes.MapRoute(
                 "Default", // Route name
                 "{controller}/{action}/{id}", // URL with parameters
-                new { controller = "Home", action = "Index", id = UrlParameter.Optional } // Parameter defaults
+                new { controller = "Post", action = "Index", id = UrlParameter.Optional } // Parameter defaults
             );
 
         }
