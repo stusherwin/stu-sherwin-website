52d51930d4b62ddd289f34d1de877c2672dc6105
stu.sherwin@googlemail.com <stu.sherwin@googlemail.com@a8c76a3a-60d2-e8ce-d006-d57739c2cf30>
2011-04-29 10:08:41 +0000

    Added CK Editor, styling

---
+++ trunk/Website/Website/Models/Metadata/Post.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace Website.Models
+{
+    [MetadataType(typeof(Metadata.Post))]
+    public partial class Post
+    { }
+}
+
+namespace Website.Models.Metadata
+{
+    public class Post
+    {
+        [UIHint("HtmlContent")]
+        public string Body { get; set; }
+    }
+
+}
---
+++ trunk/Website/Website/Scripts/HtmlContent.js
@@ -0,0 +1,13 @@
+﻿$(document).ready(function () {
+    $('.html-content-text').each(function () {
+        var contentText = $(this);
+        contentText.ckeditor(
+                function () {
+                    $('.html-content-loader').hide();
+                },
+                {
+                    height: (contentText.parent().height() - 137) + 'px',
+                    resize_enabled: false
+                });
+    });
+});
