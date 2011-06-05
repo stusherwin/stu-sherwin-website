b6745c419c40f1c7506387c4d75d8c3e3a36c193
stu.sherwin <stu.sherwin@googlemail.com>
2011-06-05 22:52:36 +0000

    Changing project structure

---
+++ Website/StuSherwin.Poco/Post.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace StuSherwin.Poco
+{
+    public class Post
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Body { get; set; }
+        public DateTime? Published { get; set; }
+        public DateTime? Updated { get; set; }
+        public ICollection<Comment> Comments { get; set; }
+    }
+}
